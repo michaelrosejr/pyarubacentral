faf69afb210c5591698ae8e82de6da80aa042bca
Michael Rose Jr <michael.rose@xome.com>
2017-11-03 22:58:48 -0700

    Convert to YAML config file Changed format of click Fixed refresh token method Add help context

---
+++ README.md
@@ -0,0 +1,14 @@
+# pyarubacentral - A Python module for accessing Aruba Central
+
+
+The following Python module assumes you have already created an API account on Aruba Central, requested a code and exchanged that code for a token. For details on how to do these steps, please follow the steps in [this guide](http://help.central.arubanetworks.com/2.2.6/documentation/online_help/content/pdfs/aruba%20central%20api%20reference%20guide.pdf)
+
+Once you have downloaded your first token, copy that token to the token.json file. Once you have the token in this file, the sampleapp.py script with refresh the token when it expires. 
+
+Basic useful switch options:
+
+ * switchlist
+ * group
+ * grouplist
+ * templatevars
+
--- sampleapp.py
+++ sampleapp.py
@@ -3,7 +3,7 @@ import requests
 import json
 import click
 import time, datetime
-from collections import OrderedDict
+import yaml
 
 from pyarubacentral import auth
 import pyarubacentral.get_switches as switches
@@ -11,72 +11,80 @@ import pyarubacentral.get_groups as groups
 import pyarubacentral.get_networks as networks
 import pyarubacentral.get_template_vars as template_vars
 
-with open('config.json', 'r') as f:
-    config = json.load(f)
+def read_config():
+    with open("config.yml", 'r') as ymlfile:
+        data = yaml.load(ymlfile)
+    return data
 
-    client_id = config["client_id"]
-    client_secret = config["client_secret"]
-    tokenfile = config["tokenfile"]
+
+# Read config file
+config = read_config()
+client_id = config["client_id"]
+client_secret = config["client_secret"]
+tokenfile = config["tokenfile"]
 
 with open(tokenfile) as exjsonfile:
     exchange_data = json.load(exjsonfile)
 
-
+current_time = time.time() * 1000
 
 refresh_token = exchange_data["refresh_token"]
 access_token = exchange_data["access_token"]
-# created_at = exchange_data["created_at"]
+created_at = exchange_data["created_at"]
 expires_in = exchange_data["expires_in"]
-expires_on = exchange_data["expires_on"]
-# print("EX On: {}".format(format(datetime.datetime.fromtimestamp(int(expires_on)).strftime('%Y-%m-%d %H:%M:%S'))))
+expires_at = ((created_at / 1000) + expires_in) * 1000
 
-if time.time() > expires_on:
-    print("Refresh me, for I have expired on {}".format(format(datetime.datetime.fromtimestamp(int(expires_on)).strftime('%Y-%m-%d %H:%M:%S'))))
+
+if current_time  > expires_at:
+    print("Refreshing token")
     url = "https://app1-apigw.central.arubanetworks.com/oauth2/token"
     payload = {'refresh_token': refresh_token, 'client_id': client_id, 'client_secret': client_secret,
                'grant_type': 'refresh_token'}
     response = requests.post(url, params=payload)
     tokenconfig = response.json()
-    tokenconfig['expires_on'] = time.time() + tokenconfig['expires_in']
+    tokenconfig['created_at'] = current_time
     with open(tokenfile, "w") as exjsonfile:
         json.dump(tokenconfig, exjsonfile)
 
+@click.group()
+def cli():
+    """CLI for Aruba Central"""
 
-@click.command()
-@click.option('--switchlist', flag_value=1)
-@click.option('--grouplist', flag_value=1)
-@click.option('--networklist', flag_value=1)
-@click.option('--refresh', flag_value=1)
-@click.option('--group', flag_value=1)
-@click.option('--templatevars')
-def app(switchlist, grouplist, networklist, refresh, group, templatevars):
-
-    if refresh:
-        url = "https://app1-apigw.central.arubanetworks.com/oauth2/token"
-        payload = {'refresh_token': refresh_token, 'client_id': client_id, 'client_secret': client_secret, 'grant_type': 'refresh_token' }
-        response = requests.post(url, params=payload)
-        tokenconfig = response.json()
-        tokenconfig['expires_on'] = time.time() + tokenconfig['expires_in']
-        with open(tokenfile, "w") as exjsonfile:
-            json.dump(response.json(), exjsonfile)
+# @cli.command()
+# @click.option('--refresh', flag_value=1)
+# def app(grouplist, networklist, refresh, group, templatevars):
+#
+#     if refresh:
+#         url = "https://app1-apigw.central.arubanetworks.com/oauth2/token"
+#         payload = {'refresh_token': refresh_token, 'client_id': client_id, 'client_secret': client_secret, 'grant_type': 'refresh_token' }
+#         response = requests.post(url, params=payload)
+#         tokenconfig = response.json()
+#         tokenconfig['created_at'] = current_time() + tokenconfig['expires_in']
+#         # with open(tokenfile, "w") as exjsonfile:
+#         #     json.dump(response.json(), exjsonfile)
 
 
-    if switchlist:
-        print(json.dumps((switches.get_switches(access_token, refresh_token))))
+@cli.command()
+def templatevars():
+    print(json.dumps((template_vars.get_template_vars(access_token, templatevars))))
 
-    if grouplist:
-        print(json.dumps((groups.get_groups(access_token))))
+@cli.command()
+def switchlist():
+    print(json.dumps((switches.get_switches(access_token, refresh_token))))
 
-    if group:
-        groupname = "Retail_AMER"
-        print(json.dumps((groups.get_group(access_token, groupname))))
+@cli.command()
+def networklist():
+    print(json.dumps((networks.get_networks(access_token))))
 
-    if networklist:
-        print(json.dumps((networks.get_networks(access_token))))
+@cli.command()
+def grouplist(grouplist):
+    print(json.dumps((groups.get_groups(access_token))))
 
-    if templatevars:
-        print(json.dumps((template_vars.get_template_vars(access_token, templatevars))))
+@cli.command()
+def group():
+    groupname = "Retail_AMER"
+    print(json.dumps((groups.get_group(access_token, groupname))))
 
 
 if __name__ == "__main__":
-    app()
+    cli()
