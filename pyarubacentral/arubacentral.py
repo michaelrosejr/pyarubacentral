e027a562eb0a068c883b49994a6e3886505a86f4
Michael Rose Jr <michael@michaelrosejr.com>
2020-06-15 12:23:19 -0700

    cleaned up code

--- pyarubacentral/arubacentral.py
+++ pyarubacentral/arubacentral.py
@@ -3,80 +3,121 @@
 import argparse
 import os, sys
 import json
+import logging
 
-parser = argparse.ArgumentParser(description='Interact with Aruba Central')
+parser = argparse.ArgumentParser(description="Interact with Aruba Central")
 # parser.add_argument('integers', metavar='N', type=int, nargs='+',
 #                     help='an integer for the accumulator')
 # parser.add_argument('--test', dest='accumulate', action='store_const',
 #                     const=sum, default=max,
 #                     help='sum the integers (default: find the max)')
-parser.add_argument('--refresh', dest='profile',
-                    help='Refresh access token for profile')
-parser.add_argument('--newtoken', dest='newtoken',
-                    help='Generate new access token for profile')
-parser.add_argument('--configure', action="store_true",
-                    help='Create configuration files for pyarubacentral module')
+parser.add_argument(
+    "--refresh", dest="profile", help="Refresh access token for profile"
+)
+parser.add_argument(
+    "--newtoken", dest="newtoken", help="Generate new access token for profile"
+)
+parser.add_argument(
+    "--configure",
+    action="store_true",
+    help="Create configuration files for pyarubacentral module",
+)
+parser.add_argument(
+    "-d",
+    "--debug",
+    help="Print lots of debugging statements",
+    action="store_const",
+    dest="loglevel",
+    const=logging.DEBUG,
+    default=logging.WARNING,
+)
+parser.add_argument(
+    "-v",
+    "--verbose",
+    help="Be verbose",
+    action="store_const",
+    dest="loglevel",
+    const=logging.INFO,
+)
 
 args = parser.parse_args()
-# print(args.configure)
+logging.basicConfig(level=args.loglevel)
 
 config_dir = os.environ["HOME"] + "/.arubacentral/"
-yaml_files = ['accounts.yml', 'regions.yml', 'config.yml']
+yaml_files = ["accounts.yml", "regions.yml", "config.yml"]
+
 
 def copy_config_files(backup):
     for conffile in yaml_files:
         print("Copying file " + conffile + " to " + config_dir + "....")
         if backup == 1:
-            os.system('mv ' + config_dir+conffile + ' ' + config_dir+conffile + '.backup')
-        os.system('wget -O ' + config_dir+conffile + ' https://raw.githubusercontent.com/michaelrosejr/pyarubacentral/master/samplescripts/' + conffile + ".sample" )
-    print("\nUse your favorite editor and edit the following files for your Aruba Central accounts:")
+            os.system(
+                "mv " + config_dir + conffile + " " + config_dir + conffile + ".backup"
+            )
+        os.system(
+            "wget -O "
+            + config_dir
+            + conffile
+            + " https://raw.githubusercontent.com/michaelrosejr/pyarubacentral/master/samplescripts/"
+            + conffile
+            + ".sample"
+        )
+    print(
+        "\nUse your favorite editor and edit the following files for your Aruba Central accounts:"
+    )
     for editconf in yaml_files:
-        print("\t" + config_dir+editconf )
+        print("\t" + config_dir + editconf)
+
 
 def check_config(args):
     if args.configure == True:
         print("Checking for existing configuration files....")
         if os.path.exists(config_dir) == False:
             print("Making directory " + config_dir)
-            os.system('mkdir -p ' + config_dir + "/tokens")
+            os.system("mkdir -p " + config_dir + "/tokens")
             fexist = False
-        
+
         for chkfile in yaml_files:
-            pwd = config_dir+chkfile
+            pwd = config_dir + chkfile
             if os.path.exists(pwd) == True:
-                print("\tFile exists: "+pwd)
+                print("\tFile exists: " + pwd)
                 fexist = True
-                  
+
             # print(os.path.exists('~/.arubacentral/accounts.yml'))
             # print(os.environ["HOME"])
         if fexist == True:
-            print("\nDo you want to overwrite these files? [Y/N]: ", end = '')
+            print("\nDo you want to overwrite these files? [Y/N]: ", end="")
             yn = str(input())
             if yn.lower() == "y":
                 copy_config_files(backup=1)
-            else: 
-                print("Exiting...\n")    
+            else:
+                print("Exiting...\n")
         else:
             copy_config_files(backup=0)
             print("\nExiting...")
 
+
 def authcode(profile):
     import pyarubacentral
+
     session = pyarubacentral.start_session(profile)
     logindata = session.get_authcode(session.get_login())
-    access_token = json.loads(session.get_access_token(logindata))['access_token']
+    access_token = json.loads(session.get_access_token(logindata))["access_token"]
     print("\nAccess Token: {}\n".format(access_token))
+
 
 def refresh(profile):
     import pyarubacentral
+
     session = pyarubacentral.start_session(profile)
     access_token = pyarubacentral.check_if_expired(profile, session)
     print("\n[{}]".format(profile))
-    print("Access Token: "+access_token+ "\n")
+    print("Access Token: " + access_token + "\n")
+
 
 def main():
     # print(args)
-    if len(sys.argv)==1:
+    if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
         sys.exit(1)
     elif args.configure == True:
@@ -87,5 +128,5 @@ def main():
         authcode(args.newtoken)
 
 
-if __name__== "__main__":
-   main()
+if __name__ == "__main__":
+    main()
--- samplescripts/exampleauth.py
+++ samplescripts/exampleauth.py
@@ -2,14 +2,28 @@ import pyarubacentral
 
 # print(pyarubacentral.Config())
 
+#
+# Set which profile to use from ~/.arubacentral/config.yaml
+#
 profile = "ACME"
 
+
+#
+# Return User List from Aruba Central
+#
+# Start the session
+session = pyarubacentral.start_session(profile)
+# Check if the current acess tokens are expired, 
+# if expired, refresh the token and continue
+# if not, continue
+access_token = pyarubacentral.check_if_expired(profile, session)
+
+# Use access tokens and get the list of users
+print(session.get_user_account_list(access_token))
+
+
+#
 ## Check when a access token expires
 # session = pyarubacentral.start_session(profile)
 # access_token = pyarubacentral.check_if_expired(profile, session)
 # print(pyarubacentral.expires(profile))
-
-# Return User List from Aruba Central
-session = pyarubacentral.start_session(profile)
-access_token = pyarubacentral.check_if_expired(profile, session)
-print(session.get_user_account_list(access_token))
